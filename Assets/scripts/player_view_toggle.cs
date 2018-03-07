99ecd5fac342f1086e99f4decc6ec8d7e6e2d161
110108 <31996737+110108@users.noreply.github.com>
2018-03-07 13:39:53 -0500

    toggle view made(dosnt work yet)

---
+++ Assets/scripts/player_view_toggle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_view_toggle : MonoBehaviour {
+
+	public GameObject v1;
+	public GameObject v3;
+	public Vector3 start;
+
+	// Use this for initialization
+	void Start () {
+		start = (0.0f , 0.0f , 0.0f);
+		v1.transform = start;
+		v3.transform = start;
+		v3.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
