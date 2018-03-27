e2bd01e3b20ee3d9fad3fc310de8fa1637e5900d
110108 <31996737+110108@users.noreply.github.com>
2018-03-27 13:31:42 -0400

    2000

--- Assets/scripts/player_view_toggle.cs
+++ Assets/scripts/player_view_toggle.cs
@@ -3,48 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class player_view_toggle : MonoBehaviour {
-	public GameObject firstPersonCamera;
-	public GameObject overheadCamera;
-
-	public void ShowOverheadView() {
-		overheadCamera.SetActive(true);
-		firstPersonCamera.SetActive(false);
-	}
-
-	public void ShowFirstPersonView() {
-		firstPersonCamera.SetActive(true);
-		overheadCamera.SetActive(false);
-	}
 
 	public GameObject v1;
 	public GameObject v3;
+	public GameObject parent;
 	public Vector3 start;
 
 	private bool threep;
 
 	// Use this for initialization
 	void Start () {
-		start = new Vector3( 81.0f , 2.5f ,55.0f );
-		v1.transform.position = start;
-		v3.transform.position = start;
+		v1.transform.localPosition = start;
+		v3.transform.localPosition = start;
 		v3.SetActive(false);
 		threep = false;
+	}
+
+	void OnTriggerEnter (Collider other){
+		if(other.gameObject.CompareTag("pickup")){
+			other.gameObject.SetActive(false);
+//			count++;
+//			SetCountText();
+		}
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (threep==false) {
-				v3.transform.position=v1.transform.position;
-			}
-		if (threep==true) {
-				v1.transform.position=v3.transform.position;
-			}
-			if (Input.GetKeyDown(KeyCode.T))
-			{
-			v3.SetActive (!v3.activeSelf);
-			v1.SetActive (!v1.activeSelf);
-			threep = !threep;
-			}
+			parent.transform.position=v1.transform.position;
+			v1.transform.localPosition = new Vector3 (0.0f, 0.0f, 0.0f);	
 		}
+		if (threep==true) {
+			parent.transform.position=v3.transform.position;
+			v3.transform.localPosition = new Vector3 (0.0f, 0.0f, 0.0f);
+		}
+		if (Input.GetKeyDown(KeyCode.T))
+		{
+		v3.SetActive (!v3.activeSelf);
+		v1.SetActive (!v1.activeSelf);
+		threep = !threep;
+		}
+	}
 }
 
